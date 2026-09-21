144572b0443aa875b02fd88e2699e6bf07b2429e
poorna92035 <117581932+poorna92035@users.noreply.github.com>
2026-09-21 04:46:52 -0400

    Create providers.tf

---
+++ prod/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.56"
+    }
+  }
+}
+
+provider "databricks" {
+  host  = var.databricks_host
+  token = var.databricks_token
+}
